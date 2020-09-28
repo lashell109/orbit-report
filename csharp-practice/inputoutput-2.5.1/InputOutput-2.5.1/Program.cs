316b518a3ef4c236d8747b461c014e662a8a05e8
La Shell Slaughter <lashellslaughter109@gmail.com>
2020-09-28 18:59:55 -0500

    Added Input Output 2.5.1

---
+++ csharp-practice/inputoutput-2.5.1/InputOutput-2.5.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InputOutput_2._5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
