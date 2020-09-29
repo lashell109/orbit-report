544141ff85aefe682011b6555f2d111b12fbe071
La Shell Slaughter <lashellslaughter109@gmail.com>
2020-09-28 19:51:20 -0500

    Added More on Numeric Types 2.5.3

---
+++ csharp-practice/moreonnumerictypes-2.5.3/MoreOnNumericTypes-2.5.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MoreOnNumericTypes_2._5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miles;
+            double gas;
+            double milesPerGallon;
+            string input;
+            
+            Console.WriteLine("How many miles you have driven?");
+            input = Console.ReadLine();
+            miles = double.Parse(input);
+
+            Console.WriteLine("How many gallons of gas have you consumed?");
+            input = Console.ReadLine();
+            gas = double.Parse(input);
+
+            milesPerGallon = miles / gas;
+            Console.WriteLine("Your miles-per-gallon is: " + milesPerGallon);
+            Console.ReadLine();
+
+        }
+    }
+}
