afa00543b682c6d2d97c21e65b4d85ea2680a7ea
La Shell Slaughter <lashellslaughter109@gmail.com>
2020-09-28 19:28:15 -0500

    Added Numeric Types 2.5.2

---
+++ csharp-practice/numerictypes-2.5.2/NumericTypes-2.5.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NumericTypes_2._5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length;
+            double width;
+            double area;
+            string input;
+
+            Console.WriteLine("What is the length of the retangle?");
+            input = Console.ReadLine();
+            length = double.Parse(input);
+
+            Console.WriteLine("What is the width of the retangle?");
+            input = Console.ReadLine();
+            width = double.Parse(input);
+
+            area = length * width;
+            Console.WriteLine("The area of the retangle is: " + area);
+            Console.ReadLine();
+        }
+    }
+}
